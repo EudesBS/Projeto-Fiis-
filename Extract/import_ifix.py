5894e0323f5987a09a39a78a0898e3244862a44b
Eudes Bezerra <60300720+EudesBS@users.noreply.github.com>
2025-02-10 17:48:27 -0300

    Update import_ifix.py

--- Extract/import_ifix.py
+++ Extract/import_ifix.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 # Dados obtidos de forma manual pelo status investing
-df_ifix = pd.read_csv("C:/Users/eudes/OneDrive/Ambiente de Trabalho/Cursos/Projeto PY/Dados Históricos - Índice Fundos de Investimentos Imobiliários.csv")
+df_ifix = pd.read_csv("seu caminho do arquivo/Projeto PY/Dados Históricos - Índice Fundos de Investimentos Imobiliários.csv")
 
 print(df_ifix)
