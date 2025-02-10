233a0dde7d5bf942b57ecebe58186bc14284f56a
Eudes Bezerra <60300720+EudesBS@users.noreply.github.com>
2025-02-10 17:46:16 -0300

    Update correcao_SELIC.py

--- Calculos/correcao_SELIC.py
+++ Calculos/correcao_SELIC.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-df_selic = pd.read_excel("C:/Users/eudes/OneDrive/Ambiente de Trabalho/Cursos/Projeto PY/Indicadores avançados/SELIC.xlsx")
+df_selic = pd.read_excel("seu caminho do arquivo")
 
 # Aplicando a interpolação linear na coluna 'Retorno Mensal SELIC'
 df_selic['Retorno Mensal SELIC'] = df_selic['Retorno Mensal SELIC'].interpolate(method='linear')
 
-df_selic.to_excel("C:/Users/eudes/OneDrive/Ambiente de Trabalho/Cursos/Projeto PY/Indicadores avançados/SELIC.xlsx")
+df_selic.to_excel("seu caminho do arquivo")
